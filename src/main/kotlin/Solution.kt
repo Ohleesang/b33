29086b72038798ce478ea789aca50a79901d2954
Ohleesang <jammmmed@gmail.com>
2024-01-24 09:57:42 +0900

    문자열 처리 문제

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,2 +1,39 @@
 class Solution {
+    fun solution(polynomial: String): String {
+        var answer: String = ""
+        val list = polynomial.split('+')
+
+        val listByX = mutableListOf<Int>()
+        val listByC = mutableListOf<Int>()
+
+        for (li in list) {
+            val str = li.replace(" ", "")
+            if (str.last() == 'x') {
+                var i = str.substring(0, str.lastIndex)
+                if(i.isBlank()) i="1"
+                listByX.add(i.toInt())
+            } else listByC.add(str.toInt())
+
+        }
+        val sumByX = listByX.sum()
+        val sumByC = listByC.sum()
+
+        if(sumByX!=0) {
+            if(sumByX == 1 ) answer = "x"
+            else answer = sumByX.toString()+"x"
+        }
+        if(sumByC!=0) {
+            if(sumByX==0) answer = sumByC.toString()
+            else answer = answer +" + "+sumByC.toString()
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    val s = Solution()
+    s.solution("3x + 7 + x")
+    s.solution("x + x + x")
+    s.solution( "1")
 }
